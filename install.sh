996c2c526b1b5081e1015fe4793c623a36a545e7
https://bnelo12@github.com <s1623919@sms.ed.ac.uk>
2018-02-05 14:50:41 +0000

    Added tmux configuration

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 # Clean up directories
 rm -rf $HOME/.vim
 rm -rf $HOME/.vimrc
+rm -rf $HOME/.tmux.conf
 
+# Install Vundle and Vim dotfiles
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 cp .vimrc $HOME/.vimrc
 vim +PluginInstall +qall
+
+# Install tmux config
+cp .tmux.conf $HOME/.tmux.conf
 
